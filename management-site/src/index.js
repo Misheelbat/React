4fe115b9f6ed231c155f9ea6dd4260bf0c843044
Misheelbat <p_misheel@me.com>
2021-12-30 21:44:57 +0100

    fixed import

--- management-site/src/index.js
+++ management-site/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import AuthcontextProvider from "./context/Authcontext";
+import {AuthcontextProvider} from "./context/Authcontext";
 
 ReactDOM.render(
   <React.StrictMode>
