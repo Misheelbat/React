8e33b2ce371d8f169b977dbabe7b24e52570046c
Misheelbat <p_misheel@me.com>
2022-03-13 20:19:43 +0100

    finished crew page

--- space-tourism/src/pages/crew/Crew.js
+++ space-tourism/src/pages/crew/Crew.js
@@ -10,38 +10,37 @@ export default function Crew() {
 
 	const crew = Data.crew;
 	const currentPage = useBgContext();
-	console.log(crew);
 
 	useEffect(() => {
-		currentPage.setPage('crew');
-	}, [currentPage.page]);
+		currentPage.setPage('crewPage');
+	}, [currentPage]);
 
 	return (
-		<div className="text-white crew-container">
-			<div>
-				<h1 className="main-title crew-title text-white">
-					<span>02</span>meet your crew
-				</h1>
+		<div className="text-white crew-container flow max-container">
+			<h1 className="main-title crew-title text-white">
+				<span>02</span>meet your crew
+			</h1>
 
+			<div className="crew-content">
 				<h2 className="crew-role">
 					<span>{crew[selectedDot].role}</span>
 					{crew[selectedDot].name}
 				</h2>
-
 				<p className="main-paragraph text-blue">{crew[selectedDot].bio}</p>
-
-				<div className="dot-indicators">
-					{crew.map((person, i) => (
-						<button
-							className={i === selectedDot ? 'active' : 0}
-							key={person.name}
-							onClick={() => setSelectedDot(i)}
-						></button>
-					))}
-				</div>
+			</div>
+			<div className="crew-nav dot-indicators">
+				{crew.map((person, i) => (
+					<button
+						className={i === selectedDot ? 'active' : 0}
+						key={person.name}
+						onClick={() => setSelectedDot(i)}
+					></button>
+				))}
 			</div>
 
-			<img className="crew-image" src={crew[selectedDot].images.png} alt="" />
+			<div className="crew-image">
+				<img src={crew[selectedDot].images.png} alt="" />
+			</div>
 		</div>
 	);
 }
